3635deac0a78159210c32fe29da7517048044cdc
mackenzie-w <mwisler@ymail.com>
2018-08-15 14:15:11 -0400

    Add project files.

---
+++ Quotably.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quotably.WebMVC.Startup))]
+namespace Quotably.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
